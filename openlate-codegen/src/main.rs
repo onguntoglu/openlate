fe62204ce3392d29480fd58d524adf920abb9125
Ongun Türkcüoglu <onguntoglu@gmail.com>
2022-10-22 13:31:57 +0200

    refactor due to renames

--- openlate-codegen/src/main.rs
+++ openlate-codegen/src/main.rs
@@ -1,33 +1,35 @@
-pub mod nidaqmx;
-use nidaqmx::metadata::NidaqmxMetadata;
+pub mod nidaqmx_gen;
+
+use nidaqmx_gen::EnumVariant;
+use nidaqmx_gen::NidaqmxMetadata;
+use nidaqmx_gen::SwapNumberToBack;
 use serde::Serialize;
-use std::env;
 use std::error::Error;
+
 use std::fs::File;
 use std::io::Write;
 use std::path::Path;
-use tera::Context;
-use tera::Tera;
 
 #[derive(Serialize, Debug)]
 struct Nidaqmx {
-  nidaqmx: NidaqmxMetadata,
+  nidaqmx: nidaqmx_gen::NidaqmxMetadata,
 }
 
 pub fn main() -> Result<(), Box<dyn Error>> {
   // Use globbing
-  let tera = match Tera::new("src/nidaqmx/templates/**/*.tera") {
+  let tera = match Tera::new("src/templates/**/*.tera") {
     Ok(t) => t,
     Err(e) => {
       println!("Parsing error(s): {}", e);
       ::std::process::exit(1);
     }
   };
+
   // Using the tera Context struct
   let mut context = Context::new();
-  let daqmx = NidaqmxMetadata::default();
+  let daqmx = nidaqmx_gen::NidaqmxMetadata::default();
   context.insert("nidaqmx", &daqmx);
-  let rendered = tera.render("values.rs.tera", &context)?;
+  let rendered = tera.render("nidaqmx_gen.rs.tera", &context)?;
 
   // let out_dir = env::var("OUT_DIR").unwrap();
   let dest_path = Path::new("nidaqmx_gen.rs");
