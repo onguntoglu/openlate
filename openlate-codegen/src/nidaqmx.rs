1dc57f6fe4d582b256465084f9f12ba949bc26c5
Ongun Türkcüoglu <onguntoglu@gmail.com>
2022-10-26 22:24:42 +0200

    nidaqmx: Attr codegen updated

--- openlate-codegen/src/nidaqmx.rs
+++ openlate-codegen/src/nidaqmx.rs
@@ -1,8 +1,9 @@
-use codegen;
+use codegen as cgen;
+use inflector::Inflector;
 use lazy_static::lazy_static;
 use regex::Regex;
 use serde::{self, Deserialize, Serialize};
-use serde_json::{self};
+use serde_json;
 use std::{
   collections::HashMap, fs::File, io::BufReader, ops::Deref, path::PathBuf,
 };
@@ -11,47 +12,117 @@ lazy_static! {
   static ref NUMBER_AT_START: Regex = Regex::new(r"^(\d+)").unwrap();
 }
 
+fn typedef(r#type: impl Into<String>) -> cgen::Type {
+  cgen::Type::new(r#type)
+}
+
+fn gen_attr_with_payload(
+  attr: &str,
+  target: &str,
+  block: cgen::Block,
+) -> cgen::Impl {
+  let mut impl_from = cgen::Impl::new(typedef(attr));
+  let fn_from = cgen::Function::new("value")
+    .vis("const")
+    .arg_self()
+    .ret(typedef(target))
+    .push_block(block)
+    .to_owned();
+  return impl_from.push_fn(fn_from).to_owned();
+}
+
+fn generate_impl_from(
+  generic: &str,
+  target: &str,
+  block: cgen::Block,
+) -> cgen::Impl {
+  let mut impl_from =
+    cgen::Impl::new(typedef(format!("From<{}> for {}", generic, target)));
+  let fn_from = cgen::Function::new("from")
+    .arg("val", typedef(generic))
+    .ret(typedef(target))
+    .push_block(block)
+    .to_owned();
+  return impl_from.push_fn(fn_from).to_owned();
+}
+
 pub struct NidaqmxGen {
-  scope: codegen::Scope,
+  scope: cgen::Scope,
   nidaqmx: NidaqmxMetadata,
 }
 
 impl NidaqmxGen {
   pub fn new() -> NidaqmxGen {
     NidaqmxGen {
-      scope: codegen::Scope::new(),
+      scope: cgen::Scope::new(),
       nidaqmx: NidaqmxMetadata::default(),
     }
     .generate_enums()
+    .generate_attrs()
   }
-  fn generate_impl_from(
-    &self,
-    generic: &str,
-    target: &str,
-    block: codegen::Block,
-  ) -> codegen::Impl {
-    let mut impl_from = codegen::Impl::new(codegen::Type::new(format!(
-      "From<{}> for {}",
-      generic, target
-    )));
-    let fn_from = codegen::Function::new("from")
-      .arg("val", codegen::Type::new(generic))
-      .ret(codegen::Type::new(target))
-      .push_block(block)
+
+  fn generate_attrs(mut self) -> NidaqmxGen {
+    let attr_it = self.nidaqmx.attr.0.drain();
+    for attr in attr_it {
+      let (attr_name, attr_values) =
+        (attr.0.clone().to_string().to_pascal_case(), attr.1.clone());
+      let mut curr_enm = cgen::Enum::new(attr_name.to_owned());
+      let mut inner_block = cgen::Block::new("match self");
+
+      for content in attr_values.0.iter() {
+        let (_, attr_property) = content;
+        let attr_variant = attr_property.name.to_pascal_case().to_owned();
+        let var = cgen::Variant::new(attr_variant.to_owned());
+        let tn = <(&str, &str)>::from(attr_property.r#type.to_owned());
+        curr_enm.push_variant(var);
+        inner_block.line(format!(
+          "{} => AttrStruct {{ access: {}, resettable: {}, ty: [{};{}] }},",
+          attr_variant,
+          String::from(AttrAccessEnum::from(attr_property.access.0.to_owned()))
+            .to_owned(),
+          attr_property.resettable.to_owned(),
+          tn.0,
+          tn.1
+        ));
+      }
+      let block_const_match = cgen::Block::new("")
+        .line(&format!("use {}::*;", attr_name).to_owned())
+        .push_block(inner_block)
+        .to_owned();
+      let impl_const_ifmatch =
+        gen_attr_with_payload(&attr_name, "AttrStruct", block_const_match);
+      self
+        .scope
+        .push_enum(curr_enm.vis("pub").to_owned())
+        .push_impl(impl_const_ifmatch.to_owned());
+    }
+    let access_enum = cgen::Enum::new("AttrAccess")
+      .push_variant(cgen::Variant::new("Read"))
+      .push_variant(cgen::Variant::new("Write"))
+      .push_variant(cgen::Variant::new("ReadWrite"))
       .to_owned();
-    return impl_from.push_fn(fn_from).to_owned();
+    let attr_struct = cgen::Struct::new("AttrStruct")
+      .generic("T")
+      .generic("const N: usize")
+      .push_field(cgen::Field::new("access", typedef("AttrAccess")))
+      .push_field(cgen::Field::new("resettable", "bool"))
+      .push_field(cgen::Field::new("ty", "[T; N]"))
+      .to_owned();
+    self.scope.push_enum(access_enum);
+    self.scope.push_struct(attr_struct);
+    self
   }
 
   fn generate_enums(mut self) -> NidaqmxGen {
     for enm in self.nidaqmx.enums.iter() {
       let (enum_name, enm_values) = (enm.0.to_string(), enm.1);
-      let mut curr_enm = codegen::Enum::new(enum_name.to_owned());
-      let mut block_raw = codegen::Block::new("match val");
-      let mut block_enum = codegen::Block::new("match val");
+      let mut curr_enm = cgen::Enum::new(enum_name.to_owned());
+      let mut block_raw = cgen::Block::new("match val");
+      let mut block_enum = cgen::Block::new("match val");
 
       for variant in enm_values.iter() {
-        let fixed_var = variant.clone().prefix_letter();
-        let mut var = codegen::Variant::new(fixed_var.to_owned().name);
+        let fixed_var = variant.clone().prefix_letter("d");
+        let mut var = cgen::Variant::new(fixed_var.name.to_pascal_case());
         match fixed_var.documentation {
           Some(doc) => var.annotation(doc.add_quotes().description),
           None => &mut var,
@@ -59,15 +130,19 @@ impl NidaqmxGen {
         curr_enm.push_variant(var);
         block_raw.line(format!(
           "{}_i32 => {}::{},",
-          fixed_var.value, enum_name, fixed_var.name
+          fixed_var.value,
+          enum_name,
+          fixed_var.name.to_pascal_case()
         ));
         block_enum.line(format!(
           "{}::{} => {}_i32,",
-          enum_name, fixed_var.name, fixed_var.value
+          enum_name,
+          fixed_var.name.to_pascal_case(),
+          fixed_var.value
         ));
       }
-      let fr_raw = self.generate_impl_from("i32", &enum_name, block_raw);
-      let fr_enum = self.generate_impl_from(&enum_name, "i32", block_enum);
+      let fr_raw = generate_impl_from("i32", &enum_name, block_raw);
+      let fr_enum = generate_impl_from(&enum_name, "i32", block_enum);
       self
         .scope
         .push_enum(curr_enm.vis("pub").to_owned())
@@ -122,6 +197,8 @@ pub struct ParameterSize {
 
 #[derive(Hash, Default, Eq, PartialEq, Debug, Serialize, Deserialize)]
 pub struct EnumName(String);
+#[derive(Hash, Default, Eq, PartialEq, Debug, Serialize, Deserialize)]
+pub struct AttrName(String);
 
 impl Deref for EnumMetadata {
   type Target = HashMap<EnumName, EnumValues>;
@@ -130,8 +207,21 @@ impl Deref for EnumMetadata {
     &self.0 //pointer to Inner value
   }
 }
+impl Deref for AttrMetadata {
+  type Target = HashMap<AttrName, AttrContent>;
+
+  fn deref(&self) -> &Self::Target {
+    &self.0 //pointer to Inner value
+  }
+}
 
 impl Deref for EnumName {
+  type Target = String;
+  fn deref(&self) -> &Self::Target {
+    &self.0 //pointer to Inner value
+  }
+}
+impl Deref for AttrName {
   type Target = String;
   fn deref(&self) -> &Self::Target {
     &self.0 //pointer to Inner value
@@ -182,7 +272,7 @@ pub struct EnumVariant {
 #[serde(rename_all = "PascalCase")]
 pub struct EnumMetadata(HashMap<EnumName, EnumValues>);
 
-#[derive(Hash, Eq, PartialEq, Debug, Serialize, Deserialize)]
+#[derive(Hash, Clone, Eq, PartialEq, Debug, Serialize, Deserialize)]
 pub struct Integer(String);
 
 impl From<Integer> for i32 {
@@ -193,22 +283,118 @@ impl From<Integer> for i32 {
   }
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct AttrContent(HashMap<Integer, AttrProperty>);
+impl Deref for AttrContent {
+  type Target = HashMap<Integer, AttrProperty>;
+
+  fn deref(&self) -> &Self::Target {
+    &self.0 //pointer to Inner value
+  }
+}
 
 #[derive(Debug, Serialize, Deserialize)]
 #[serde(rename_all = "PascalCase")]
-pub struct AttrMetadata(HashMap<String, AttrContent>);
+pub struct AttrMetadata(HashMap<AttrName, AttrContent>);
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Clone, Debug, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct AttrProperty {
-  access: String,
+  access: AttrAccess,
   name: String,
+  r#enum: Option<AttrEnum>,
   resettable: bool,
-  r#type: String,
+  r#type: AttrType,
 }
 
+#[derive(Hash, Clone, Eq, PartialEq, Debug, Serialize, Deserialize)]
+pub struct AttrAccess(String);
+#[derive(Hash, Clone, Eq, PartialEq, Debug, Serialize, Deserialize)]
+pub struct AttrResettable(String);
+#[derive(Hash, Clone, Eq, PartialEq, Debug, Serialize, Deserialize)]
+pub struct AttrType(String);
+#[derive(Hash, Clone, Eq, PartialEq, Debug, Serialize, Deserialize)]
+pub struct AttrEnum(String);
+
+impl From<AttrAccessEnum> for String {
+  fn from(val: AttrAccessEnum) -> String {
+    match val {
+      AttrAccessEnum::Read => "AttrAccess::Read",
+      AttrAccessEnum::Write => "AttrAccess::Write",
+      AttrAccessEnum::ReadWrite => "AttrAccess::ReadWrite",
+    }
+    .to_string()
+  }
+}
+
+enum AttrAccessEnum {
+  Read,
+  Write,
+  ReadWrite,
+}
+impl From<AttrType> for (&str, &str) {
+  fn from(val: AttrType) -> Self {
+    match val.0.as_str() {
+      "int8" => ("i8", "0"),
+      "uInt8" => ("u8", "0"),
+      "int16" => ("i16", "0"),
+      "uInt16" => ("u16", "0"),
+      "int32" => ("i32", "0"),
+      "uInt32" => ("u32", "0"),
+      "float32" => ("f32", "0"),
+      "float64" => ("f64", "0"),
+      "int64" => ("i64", "0"),
+      "uInt64" => ("u64", "0"),
+      "bool32" => ("bool", "0"),
+      "CVIAbsoluteTime" => ("CVIAbsoluteTime", "0"),
+      "int8[]" => ("i8", "N"),
+      "uInt8[]" => ("u8", "N"),
+      "int16[]" => ("i16", "N"),
+      "uInt16[]" => ("u16", "N"),
+      "int32[]" => ("i32", "N"),
+      "uInt32[]" => ("u32", "N"),
+      "float32[]" => ("f32", "N"),
+      "float64[]" => ("f64", "N"),
+      "int64[]" => ("i64", "N"),
+      "uInt64[]" => ("u64", "N"),
+      "char[]" => ("i8", "N"),
+      _ => panic!("Missing match arm detected!"),
+    }
+  }
+}
+
+impl From<String> for AttrAccessEnum {
+  fn from(val: String) -> AttrAccessEnum {
+    match val.as_str() {
+      "read" => AttrAccessEnum::Read,
+      "write" => AttrAccessEnum::Write,
+      "read-write" => AttrAccessEnum::ReadWrite,
+      _ => panic!("Invalid AttrAccess variant"),
+    }
+  }
+}
+
+impl Deref for AttrAccess {
+  type Target = String;
+
+  fn deref(&self) -> &Self::Target {
+    &self.0 //pointer to Inner value
+  }
+}
+impl Deref for AttrResettable {
+  type Target = String;
+
+  fn deref(&self) -> &Self::Target {
+    &self.0 //pointer to Inner value
+  }
+}
+impl Deref for AttrType {
+  type Target = String;
+
+  fn deref(&self) -> &Self::Target {
+    &self.0 //pointer to Inner value
+  }
+}
 impl Default for NidaqmxMetadata {
   fn default() -> NidaqmxMetadata {
     let metadata_dir = PathBuf::from("metadata/nidaqmx/");
@@ -234,9 +420,9 @@ impl Default for NidaqmxMetadata {
 }
 
 impl EnumProcessor for EnumVariant {
-  fn prefix_letter(mut self) -> Self {
+  fn prefix_letter(mut self, letter: &str) -> Self {
     let foo = match self.name.as_bytes()[0].is_ascii_digit() {
-      true => "d".to_owned() + &self.name,
+      true => letter.to_owned() + &self.name,
       false => self.name,
     };
     self.name = foo;
@@ -267,7 +453,7 @@ impl EnumProcessor for EnumVariant {
 
 pub trait EnumProcessor {
   fn swap_number(self) -> Self;
-  fn prefix_letter(self) -> Self;
+  fn prefix_letter(self, letter: &str) -> Self;
 }
 
 #[cfg(test)]
